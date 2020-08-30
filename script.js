11b5d761cfdbbd84e21dcb3dca183fd698ff9b86
Nauman Munir <naumanmunir@gmail.com>
2020-08-30 13:03:49 +0500

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const navbarBtn = document.querySelector(".navbar__btn");
-const navbarLinks = document.querySelector(".navbar__links");
+const navbarBtn = document.querySelector(".navbar_btn");
+const navbarLinks = document.querySelector(".navbar_links");
 
 navbarBtn.addEventListener("click", function () {
-  let value = navbarLinks.classList.contains("navbar__collapse");
+  let value = navbarLinks.classList.contains("navbar_collapse");
 
   if (value) {
-    navbarLinks.classList.remove("navbar__collapse");
+    navbarLinks.classList.remove("navbar_collapse");
     navbarBtn.classList.remove("change");
   } else {
-    navbarLinks.classList.add("navbar__collapse");
+    navbarLinks.classList.add("navbar_collapse");
     navbarBtn.classList.add("change");
   }
 });
